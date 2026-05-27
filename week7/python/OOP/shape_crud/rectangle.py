a79b2850f117daf78c39ab7456fadd5899a1a983
david999meir-png <david999meir@gmail.com>
2026-05-27 09:10:17 +0300

    shape_crud: build the Rectangle class

---
+++ week7/python/OOP/shape_crud/rectangle.py
@@ -0,0 +1,27 @@
+from shape import Shape
+
+
+class Rectangle(Shape):
+    def __init__(self, shape_id, shape_type, width, height):
+        super().__init__(shape_id, shape_type)
+        self.width = width
+        self.height = height
+
+    
+    @property
+    def get_area(self):
+        return self.width * self.height
+    
+    @property
+    def get_perimeter(self):
+        return (self.height*2) + (self.width*2)
+    
+    def to_dict(self):
+        object_dict = {
+                        "id": self.id,
+                       "type": self.shape_type,
+                       "width": self.width,
+                       "hight": self.height
+                        }  
+        return object_dict 
+    

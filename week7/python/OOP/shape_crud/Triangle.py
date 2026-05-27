3c24956d650e9de2ce1403cbaf5061e96deec3cb
david999meir-png <david999meir@gmail.com>
2026-05-27 09:40:00 +0300

    shape_crud: build the Tringle class

---
+++ week7/python/OOP/shape_crud/Triangle.py
@@ -0,0 +1,24 @@
+from shape import Shape
+
+
+class Tringle(Shape):
+    def __init__(self, shape_id, shape_type, side):
+        super().__init__(shape_id, shape_type)
+        self.side = side
+    
+    @property
+    def get_area(self):
+        return (3**0.5/4) * (self.side**2)
+    
+    @property
+    def get_perimeter(self):
+        return self.side * 3
+    
+    def to_dict(self):
+        object_dict = {
+                        "id": self.id,
+                       "type": self.shape_type,
+                       "side": self.side
+                        }  
+        return object_dict 
+    

8f837d1cabc897adac8485cad7b3bdca6c4b623e
david999meir-png <david999meir@gmail.com>
2026-05-27 08:26:32 +0300

    shape_crud: build the Circle class

---
+++ week7/python/OOP/shape_crud/circle.py
@@ -0,0 +1,25 @@
+from shape import Shape
+
+
+class Circle(Shape):
+    pay = 3.14159
+    def __init__(self, shape_id, shape_type, radius):
+        super().__init__(shape_id, shape_type)
+        self.radius = radius
+    
+    @property
+    def get_area(self):
+        return self.radius ** 2 * Circle.pay
+    
+    @property
+    def get_perimeter(self):
+        return 2 * Circle.pay * self.radius
+    
+    def to_dict(self):
+        object_dict = {
+                        "id": self.id,
+                       "type": self.shape_type,
+                       "radius": self.radius
+                        }  
+        return object_dict 
+    

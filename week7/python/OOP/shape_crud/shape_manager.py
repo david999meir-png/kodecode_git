532ee478f635e89d058ed0491988b51d8cba69b1
david999meir-png <david999meir@gmail.com>
2026-05-27 11:55:51 +0300

    shape_crud: build the Shape Maneger class base

---
+++ week7/python/OOP/shape_crud/shape_manager.py
@@ -0,0 +1,27 @@
+from shape import Shape
+
+
+class ShapeManager:
+    counter_id = 0
+
+    def __init__(self):
+        self.shapes = [Shape]
+        self.load_from_json()
+
+    def create_shape(self, shape):        
+        pass
+    
+    def get_all_shapes(self):
+        pass
+    
+    def update_shape(self, shape_id, new_data):
+        pass
+    
+    def delete_shape(self, shape_id):
+        pass
+    
+    def save_to_json(self):
+        pass
+
+    def load_from_json(self):
+        pass
